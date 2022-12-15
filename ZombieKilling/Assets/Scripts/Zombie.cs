496d55226a95afe6ab7d0df017082deed4383901
itsAdi <taditya1@hotmail.com>
2022-12-15 21:36:16 +0530

    Added zombie patroling
    Added zombie patroling

--- ZombieKilling/Assets/Scripts/Zombie.cs
+++ ZombieKilling/Assets/Scripts/Zombie.cs
@@ -7,6 +7,8 @@ public class Zombie : MonoBehaviour
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private ZombieAnimator _zombieAnimator;
     [SerializeField] private float _walkSpeed, _runSpeed;
+    [SerializeField] private float _patrolDestinationRadius;
+    [SerializeField] private Vector2 _minMaxPatrolRerouteTime;
 
     private bool _playerFound;
 
@@ -22,7 +24,6 @@ public class Zombie : MonoBehaviour
     {
         if(_playerFound)
         {
-            _navMeshAgent.SetDestination(SingletonManager.Instance.Player.transform.position);
             EvaluateAgentState();
         }
     }
@@ -35,6 +36,18 @@ public class Zombie : MonoBehaviour
         if(oldState != _agentState) { _zombieAnimator.OnAgentStateUpdated(_agentState); }
     }
 
+    private bool GetRandomNavmeshDestination(out Vector3 destinationPoint)
+    {
+        Vector3 sourcePosition = transform.position + Random.insideUnitSphere * _patrolDestinationRadius;
+        if(NavMesh.SamplePosition(sourcePosition, out NavMeshHit hit, 2f, NavMesh.AllAreas))
+        {
+            destinationPoint = hit.position;
+            return true;
+        }
+        destinationPoint = Vector3.zero;
+        return false;
+    }
+
     IEnumerator DetectPlayer()
     {
         while (SingletonManager.Instance.Player == null)
@@ -42,5 +55,22 @@ public class Zombie : MonoBehaviour
             yield return null;
         }
         _playerFound = true;
+        StartCoroutine(SetPatrolRoute());
+    }
+
+    IEnumerator SetPatrolRoute()
+    {
+        bool canStopSearching = false;
+        Vector3 patrolDestination = Vector3.zero;
+        while(!canStopSearching)
+        {
+            canStopSearching = GetRandomNavmeshDestination(out patrolDestination);
+            yield return null;
+        }
+        Debug.DrawRay(patrolDestination, Vector3.up, Color.red, 10f);
+        _navMeshAgent.SetDestination(patrolDestination);
+        float reRouteTime = Time.time + Random.Range(_minMaxPatrolRerouteTime.x, _minMaxPatrolRerouteTime.y);
+        yield return new WaitUntil(()=>Time.time > reRouteTime);
+        StartCoroutine(SetPatrolRoute());
     }
 }
