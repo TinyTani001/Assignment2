56c3b05b84efe339ad0f7621493186cabb06048c
itsAdi <taditya1@hotmail.com>
2022-12-21 22:30:07 +0530

    Zombie can detect player
    Zombies now can detect player if it is in detection range

--- ZombieKilling/Assets/Scripts/Zombie.cs
+++ ZombieKilling/Assets/Scripts/Zombie.cs
@@ -1,23 +1,29 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class Zombie : MonoBehaviour
 {
+    [SerializeField] private bool _showDebugGizmos;
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private ZombieAnimator _zombieAnimator;
     [SerializeField] private float _walkSpeed, _runSpeed;
     [SerializeField] private float _patrolDestinationRadius;
     [SerializeField] private Vector2 _minMaxPatrolRerouteTime;
+    [SerializeField, Range(0f, 1f)] private float _playerDetectionRange = 0.2f;
 
     private bool _playerFound;
 
     private int _agentState;
+    private float _playerDetectionDotProduct;
 
     private void Start()
     {
         StartCoroutine(DetectPlayer());
         _navMeshAgent.speed = _walkSpeed;
+        _playerDetectionDotProduct = Vector3.Dot(transform.forward, Quaternion.AngleAxis(Mathf.Lerp(0f, 180f, _playerDetectionRange), Vector3.up) * transform.forward);
     }
 
     private void FixedUpdate()
@@ -25,6 +31,41 @@ public class Zombie : MonoBehaviour
         if(_playerFound)
         {
             EvaluateAgentState();
+
+            Vector3 zombiePosition = transform.position;
+
+            Vector3 flatPlayerPosition =  SingletonManager.Instance.Player.transform.position;
+            flatPlayerPosition.y = zombiePosition.y;
+            Vector3 zombieToPlayerDirection = (flatPlayerPosition - zombiePosition).normalized;
+            Debug.DrawRay(transform.position, transform.forward, Color.red);
+            Debug.DrawRay(transform.position, zombieToPlayerDirection, Color.green);
+            float dot = Vector3.Dot(transform.forward, zombieToPlayerDirection);
+            if (dot >= _playerDetectionDotProduct && (zombiePosition - flatPlayerPosition).sqrMagnitude < _patrolDestinationRadius * _patrolDestinationRadius)
+            {
+                Debug.Log("Player detected");
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!_showDebugGizmos) return;
+        Gizmos.color = Color.green;
+        Vector3 dir = transform.forward;
+        Vector3 startPosition = transform.position;
+        Gizmos.DrawLine(startPosition, startPosition + (Quaternion.AngleAxis(Mathf.Lerp(0f, 180f, _playerDetectionRange), Vector3.up) * dir * _patrolDestinationRadius));
+        Gizmos.DrawLine(startPosition, startPosition + (Quaternion.AngleAxis(Mathf.Lerp(360f, 180f, _playerDetectionRange), Vector3.up) * dir * _patrolDestinationRadius));
+        Vector3 origin = transform.position;
+        Vector3 startRotation = transform.right * _patrolDestinationRadius;
+        Vector3 lastPosition = origin + startRotation;
+        float angle = 0;
+        while (angle <= 360)
+        {
+            angle += 360 / 20;
+            Vector3 nextPosition = origin + (Quaternion.Euler(0, angle, 0) * startRotation);
+            Gizmos.DrawLine(lastPosition, nextPosition);
+
+            lastPosition = nextPosition;
         }
     }
 
@@ -55,7 +96,7 @@ public class Zombie : MonoBehaviour
             yield return null;
         }
         _playerFound = true;
-        StartCoroutine(SetPatrolRoute());
+        //StartCoroutine(SetPatrolRoute());
     }
 
     IEnumerator SetPatrolRoute()
@@ -67,7 +108,6 @@ public class Zombie : MonoBehaviour
             canStopSearching = GetRandomNavmeshDestination(out patrolDestination);
             yield return null;
         }
-        Debug.DrawRay(patrolDestination, Vector3.up, Color.red, 10f);
         _navMeshAgent.SetDestination(patrolDestination);
         float reRouteTime = Time.time + Random.Range(_minMaxPatrolRerouteTime.x, _minMaxPatrolRerouteTime.y);
         yield return new WaitUntil(()=>Time.time > reRouteTime);
